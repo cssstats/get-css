a97cde8058ab266216aa418594393b5affd6f7d8
John Otander <johnotander@gmail.com>
2014-12-01 22:51:58 -0700

    Remove extra semicolon

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ module.exports = function(url, options){
           link.error = error;
           parsed++;
           handleResolve();
-        });;
+        });
     });
     result.styles.forEach(function(css) {
       result.css += css;
