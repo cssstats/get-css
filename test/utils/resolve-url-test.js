c9f24d17cd8fe3633a6427eb5b000a3b66d3027f
John Otander <johnotander@gmail.com>
2014-12-01 23:24:02 -0700

    Add a few more url edge cases

--- test/utils/resolve-url-test.js
+++ test/utils/resolve-url-test.js
@@ -3,8 +3,16 @@ var resolveUrl = require('../../utils/resolve-url');
 
 describe('resolve-url', function() {
 
-  it('should correctly resolve a relative link', function() {
+  it('should correctly resolve a .. relative link', function() {
     assert.equal(resolveUrl('http://foo.com/some/path', '../bar.css'), 'http://foo.com/some/bar.css');
+  });
+
+  it('should correctly resolve a .. relative link when the url has a trailing /', function() {
+    assert.equal(resolveUrl('http://foo.com/some/path/', '../bar.css'), 'http://foo.com/some/bar.css');
+  });
+
+  it('should correctly resolve a relative link', function() {
+    assert.equal(resolveUrl('http://foo.com/some/path', 'bar.css'), 'http://foo.com/some/path/bar.css');
   });
 
   it('should correctly return a full link', function() {
