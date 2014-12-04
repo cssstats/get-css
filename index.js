3f3dcddcfcd0b9cde166483412e1b06795d1b1d7
John Otander <johnotander@gmail.com>
2014-12-03 22:16:42 -0700

    Ensure that all levels of imports are handled

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ module.exports = function(url, options){
   var deferred = q.defer();
 
   var options = options || {};
-  var parsed = 0;
-  var total = 0;
+
+  var status = {
+    parsed: 0,
+    total: 0
+  }
 
   var result = {
     links: [],
@@ -25,7 +28,7 @@ module.exports = function(url, options){
   options.timeout = options.timeout || 5000;
 
   function handleResolve() {
-    if (parsed >= total) {
+    if (status.parsed >= status.total) {
       deferred.resolve(result);
     }
   }
@@ -37,54 +40,67 @@ module.exports = function(url, options){
     $('[rel=stylesheet]').each(function() {
       var link = $(this).attr('href');
       var resolvedUrl = resolveUrl(url, link);
-      result.links.push({ link: link, url: resolvedUrl });
+      result.links.push({ link: link, url: resolvedUrl, css: '' });
     });
 
     $('style').each(function() {
       result.styles.push( $(this).text() );
     });
 
-    total = result.links.length + result.styles.length;
+    status.total = result.links.length + result.styles.length;
+    if (!status.total) {
+      deferred.resolve(false);
+    }
 
-    if (!total) deferred.resolve(false);
     result.links.forEach(function(link) {
       getLinkContents(link.url, options)
         .then(function(css) {
-          link.css = css;
-          result.css += css;
-
-          if (options.followImports) {
-            // Handle potential @import url(foo.css) statements in the CSS.
-            link.imports = resolveCssImportUrls(link.url, css);
-            total += link.imports.length;
-            link.imports.forEach(function(importUrl) {
-              getLinkContents(importUrl, options)
-                .then(function(css) {
-                  result.css += css;
-                  parsed++;
-                  handleResolve();
-                })
-                .catch(function(error) {
-                  link.error = error;
-                  parsed++;
-                  handleResolve();
-                });
-            });
-          }
-
-          parsed++;
-          handleResolve();
+          handleCssFromLink(link, css);
         })
         .catch(function(error) {
           link.error = error;
-          parsed++;
+          status.parsed++;
           handleResolve();
         });
     });
+
     result.styles.forEach(function(css) {
       result.css += css;
-      parsed++;
+      status.parsed++;
       handleResolve();
+    });
+  }
+
+  function handleCssFromLink(link, css) {
+    link.css += css;
+
+    if (options.followImports) {
+      parseCssForImports(link, css);
+    } else {
+      result.css += css;
+    }
+
+    status.parsed++;
+    handleResolve();
+  }
+
+  // Handle potential @import url(foo.css) statements in the CSS.
+  function parseCssForImports(link, css) {
+    link.imports = resolveCssImportUrls(link.url, css);
+    status.total += link.imports.length;
+    result.css += css;
+
+    link.imports.forEach(function(importUrl) {
+      getLinkContents(importUrl, options)
+        .then(function(css) {
+          result.css += css;
+          handleCssFromLink(link, css);
+        })
+        .catch(function(error) {
+          link.error = error;
+          status.parsed++;
+          handleResolve();
+        });
     });
   }
 
