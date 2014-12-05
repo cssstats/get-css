6b071573534a7059d42f2703997f4fb5e0af2a15
John Otander <johnotander@gmail.com>
2014-12-05 09:46:33 -0700

    Make resolve-url more robust

--- utils/resolve-url.js
+++ utils/resolve-url.js
@@ -4,10 +4,24 @@ module.exports = function resolveUrl(url, link) {
   if(link.match(/^(http|https)/g)) {
     return link;
   } else {
-    if(url.indexOf('/', url.length - '/'.length) == -1) {
-      url = url + '/';
+    if(isCssFile(url)) {
+      removeExtension(url);
+    } else if(!endsInForwardSlash(url)) {
+      url += '/';
     }
 
     return urlResolver(url, link);
   }
 };
+
+function endsInForwardSlash(url) {
+  return url.indexOf('/', url.length - 1) != -1;
+}
+
+function isCssFile(url) {
+  return url.indexOf('.css', url.length - 4) != -1;
+}
+
+function removeExtension(url) {
+  url.replace(/\.[^/.]+$/, '');
+}
