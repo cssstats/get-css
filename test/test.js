42780e0b1c6fbf8d290c0483a45b40dadf255a5c
John Otander <johnotander@gmail.com>
2015-08-13 09:06:25 -0600

    Test direct linked css

--- test/test.js
+++ test/test.js
@@ -4,16 +4,16 @@ var getCss = require('../');
 
 var results = [];
 
-function writeLog() {
-  fs.writeFileSync('./test/results.json', JSON.stringify(results, null, 2) );
+function writeLog () {
+  fs.writeFileSync('./test/results.json', JSON.stringify(results, null, 2));
 }
 
-function logResults(response) {
+function logResults (response) {
   results.push(response);
   console.log('Results from: ', response.pageTitle);
   console.log(response.styles.length + ' Style Tags');
   console.log(response.links.length + ' Stylesheets');
-  response.links.forEach(function(link) {
+  response.links.forEach(function (link) {
     console.log(link.url);
   });
   writeLog();
@@ -21,24 +21,30 @@ function logResults(response) {
 
 getCss('http://google.com')
   .then(logResults)
-  .catch(function(err){
+  .catch(function (err) {
     console.error(err);
   });
 
 getCss('http://amazon.com')
   .then(logResults)
-  .catch(function(err){
+  .catch(function (err) {
     console.error(err);
   });
 
 getCss('http://twitter.com/jxnblk')
   .then(logResults)
-  .catch(function(err){
+  .catch(function (err) {
     console.error(err);
   });
 
 getCss('http://facebook.com')
   .then(logResults)
-  .catch(function(err){
+  .catch(function (err) {
+    console.error(err);
+  });
+
+getCss('http://johnotander.com/public/css/c.min.css')
+  .then(logResults)
+  .catch(function (err) {
     console.error(err);
   });
