113c9821bfe2a55b879c8f55e0376a1042b4c446
Autarc <autarc@gmail.com>
2016-05-24 13:49:07 +0100

    use only CSS content

--- utils/get-link-contents.js
+++ utils/get-link-contents.js
@@ -4,6 +4,12 @@ var request = require('request');
 module.exports = function getLinkContents(linkUrl, options) {
   var d = q.defer();
 
+  // expect linked css content
+  if (!/\.css$/i.test(linkUrl)) {
+    d.resolve('');
+    return d.promise;
+  }
+
   request({ url: linkUrl, timeout: options.timeout, gzip: true }, function(error, response, body) {
     if (error || response.statusCode !== 200) {
       d.reject(error);
