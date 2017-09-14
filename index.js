c015fa7c34fa276566215aa4fed134eea25cace5
fabs <faebeee@gmail.com>
2017-09-14 21:53:23 +0200

    fix inline style

--- index.js
+++ index.js
@@ -14,135 +14,135 @@ var ua = require('ua-string');
 var getLinkContents = require('./utils/get-link-contents');
 var createLink = require('./utils/create-link');
 
-module.exports = function (url, options) {
-    var deferred = q.defer();
-    var options = options || {};
-    options.headers = options.headers || {};
-    options.headers['User-Agent'] = options.headers['User-Agent'] || ua;
-    options.timeout = options.timeout || 5000;
-    options.gzip = true;
+module.exports = function(url, options){
+  var deferred = q.defer();
+  var options = options || {};
+  options.headers = options.headers || {};
+  options.headers['User-Agent'] = options.headers['User-Agent'] || ua;
+  options.timeout = options.timeout || 5000;
+  options.gzip = true;
 
-    if (typeof url !== 'string' || isBlank(url) || !isUrl(url)) {
-        throw new TypeError('get-css expected a url as a string')
+  if (typeof url !== 'string' || isBlank(url) || !isUrl(url)) {
+    throw new TypeError('get-css expected a url as a string')
+  }
+
+  url = normalizeUrl(url, { stripWWW: false });
+  options.url = url;
+
+  if (options.ignoreCerts) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  }
+
+  var status = {
+    parsed: 0,
+    total: 0
+  };
+
+  var result = {
+    links: [],
+    styles: [],
+    css: ''
+  };
+
+  function handleResolve() {
+    if (status.parsed >= status.total) {
+      deferred.resolve(result);
     }
+  }
 
-    url = normalizeUrl(url, {stripWWW: false});
-    options.url = url;
+  function parseHtml(html) {
+    var $ = cheerio.load(html);
+    result.pageTitle = $('head > title').text();
 
-    if (options.ignoreCerts) {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-    }
-
-    var status = {
-        parsed: 0,
-        total: 0
-    };
-
-    var result = {
-        links: [],
-        styles: [],
-        css: ''
-    };
-
-    function handleResolve() {
-        if (status.parsed >= status.total) {
-            deferred.resolve(result);
-        }
-    }
-
-    function parseHtml(html) {
-        var $ = cheerio.load(html);
-        result.pageTitle = $('head > title').text();
-
-        $('[rel=stylesheet]').each(function () {
-            var link = $(this).attr('href');
-            if (!(typeof link !== typeof undefined && link !== false)) {
-                result.styles.push(stripHtmlComments($(this).text()));
-                return;
-            }
-            result.links.push(createLink(link, url));
-        });
-
-        $('style').each(function () {
+    $('[rel=stylesheet]').each(function() {
+        var link = $(this).attr('href');
+        if (!(typeof link !== typeof undefined && link !== false)) {
             result.styles.push(stripHtmlComments($(this).text()));
-        });
-
-        status.total = result.links.length + result.styles.length;
-        if (!status.total) {
-            deferred.resolve(false);
-        }
-
-        result.links.forEach(function (link) {
-            getLinkContents(link.url, options)
-                .then(function (css) {
-                    handleCssFromLink(link, css);
-                })
-                .catch(function (error) {
-                    link.error = error;
-                    status.parsed++;
-                    handleResolve();
-                });
-        });
-
-        result.styles.forEach(function (css) {
-            result.css += css;
-            status.parsed++;
-            handleResolve();
-        });
-    }
-
-    function handleCssFromLink(link, css) {
-        link.css += css;
-
-        parseCssForImports(link, css);
-
-        status.parsed++;
-        handleResolve();
-    }
-
-    // Handle potential @import url(foo.css) statements in the CSS.
-    function parseCssForImports(link, css) {
-        link.imports = resolveCssImportUrls(link.url, css);
-        status.total += link.imports.length;
-        result.css += css;
-
-        link.imports.forEach(function (importUrl) {
-            var importLink = createLink(importUrl, importUrl);
-            result.links.push(importLink);
-
-            getLinkContents(importLink.url, options)
-                .then(function (css) {
-                    handleCssFromLink(importLink, css);
-                })
-                .catch(function (error) {
-                    link.error = error;
-                    status.parsed++;
-                    handleResolve();
-                });
-        });
-    }
-
-    request(options, function (error, response, body) {
-        if (error) {
-            if (options.verbose) console.log('Error from ' + url + ' ' + error);
-            deferred.reject(error);
             return;
         }
-
-        if (response && response.statusCode != 200) {
-            if (options.verbose) console.log('Received a ' + response.statusCode + ' from: ' + url);
-            deferred.reject({url: url, statusCode: response.code});
-            return;
-        }
-
-        if (isCss(url)) {
-            var link = createLink(url, url);
-            result.links.push(link);
-            handleCssFromLink(link, body);
-        } else {
-            parseHtml(body);
-        }
+      result.links.push(createLink(link, url));
     });
 
-    return deferred.promise;
+    $('style').each(function() {
+      result.styles.push(stripHtmlComments($(this).text()));
+    });
+
+    status.total = result.links.length + result.styles.length;
+    if (!status.total) {
+      deferred.resolve(false);
+    }
+
+    result.links.forEach(function(link) {
+      getLinkContents(link.url, options)
+        .then(function(css) {
+          handleCssFromLink(link, css);
+        })
+        .catch(function(error) {
+          link.error = error;
+          status.parsed++;
+          handleResolve();
+        });
+    });
+
+    result.styles.forEach(function(css) {
+      result.css += css;
+      status.parsed++;
+      handleResolve();
+    });
+  }
+
+  function handleCssFromLink(link, css) {
+    link.css += css;
+
+    parseCssForImports(link, css);
+
+    status.parsed++;
+    handleResolve();
+  }
+
+  // Handle potential @import url(foo.css) statements in the CSS.
+  function parseCssForImports(link, css) {
+    link.imports = resolveCssImportUrls(link.url, css);
+    status.total += link.imports.length;
+    result.css += css;
+
+    link.imports.forEach(function(importUrl) {
+      var importLink = createLink(importUrl, importUrl);
+      result.links.push(importLink);
+
+      getLinkContents(importLink.url, options)
+        .then(function(css) {
+          handleCssFromLink(importLink, css);
+        })
+        .catch(function(error) {
+          link.error = error;
+          status.parsed++;
+          handleResolve();
+        });
+    });
+  }
+
+  request(options, function(error, response, body) {
+    if (error) {
+      if (options.verbose) console.log('Error from ' + url + ' ' + error);
+      deferred.reject(error);
+      return;
+    }
+
+    if (response && response.statusCode != 200) {
+      if (options.verbose) console.log('Received a ' + response.statusCode + ' from: ' + url);
+      deferred.reject({ url: url, statusCode: response.code });
+      return;
+    }
+
+    if (isCss(url)) {
+      var link = createLink(url, url);
+      result.links.push(link);
+      handleCssFromLink(link, body);
+    } else {
+      parseHtml(body);
+    }
+  });
+
+  return deferred.promise;
 };
