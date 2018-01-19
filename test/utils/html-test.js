66b3171dd6b1c473dc005038c77db56b142b07b7
John Otander <johnotander@gmail.com>
2018-01-19 08:37:29 -0700

    Convert tabs to spaces

--- test/utils/html-test.js
+++ test/utils/html-test.js
@@ -5,9 +5,9 @@ var css = "h1 { color: tomato; }";
 var html = "<style>" + css + "</style><h1>Hello, world!</h1>";
 
 describe("html", function() {
-	it("should correctly extract css from raw html", function() {
-		getCss("http://example.com/", null, html).then(function(response) {
-			asset.deepEqual(css, response.css);
-		});
-	});
+  it("should correctly extract css from raw html", function() {
+    getCss("http://example.com/", null, html).then(function(response) {
+      asset.deepEqual(css, response.css);
+    });
+  });
 });
