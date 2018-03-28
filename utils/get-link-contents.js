bb20313464145b4b7db6e5f5a5f5955b52d5ace2
John Otander <johnotander@gmail.com>
2018-03-28 05:20:15 -0600

    Specify utils dir rather than lib

--- utils/get-link-contents.js
+++ utils/get-link-contents.js
@@ -1,22 +1,26 @@
-var q = require('q');
-var request = require('request');
+var q = require('q')
+var request = require('request')
 
 module.exports = function getLinkContents(linkUrl, options) {
-  var d = q.defer();
+  var d = q.defer()
 
   // expect linked css content
   if (!/\.css$/i.test(linkUrl)) {
-    d.resolve('');
-    return d.promise;
+    d.resolve('')
+    return d.promise
   }
 
-  request({ url: linkUrl, timeout: options.timeout, gzip: true }, function(error, response, body) {
+  request({ url: linkUrl, timeout: options.timeout, gzip: true }, function(
+    error,
+    response,
+    body
+  ) {
     if (error || response.statusCode !== 200) {
-      d.reject(error);
+      d.reject(error)
     }
 
-    d.resolve(body);
-  });
+    d.resolve(body)
+  })
 
-  return d.promise;
-};
+  return d.promise
+}
